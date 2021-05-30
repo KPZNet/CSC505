902d1191a9761c9ac1525d73dd31563e5918ce3c
KPZNET <kpznet@outlook.com>
2021-05-29 20:18:47 -0700

    printout

--- M3/ShoppingList.py
+++ M3/ShoppingList.py
@@ -6,100 +6,41 @@ __maintainer__ = "Kenneth Ceglia"
 __email__ = "kenceglia@gmail.com"
 __status__ = "Course Work"
 
-""" ModifiedWaterfall enables user to input
-    a framework activity by name.
-    ModifiedWaterfall returns the activity
-    definition and details in a well-formatted
-    printout
+""" Shopping List prints out
+    the psuedo code for the shopping list app
+    and the tools used to produce the design
+    and models
 """
 import json
 
 
-class ceglia:
+class ShoppingListApp:
 
-    def findActivity(self, activity, wfallmodel):
-        """
-        Searches list of activities to find
-        requested activity and return found
-        activity
 
-        :param activity: waterfall activity object
-        :param wfallmodel: input waterfall database
-        :return: activity object
-        """
-        activities = wfallmodel['Activities']
-        retActivity = None
-        for index, a in enumerate(activities):
-            act = a['Name']
-            if act.upper() == activity.upper():
-                retActivity = a
-                break
-        return retActivity
+    def GetPrintCode(self):
+        print('\nPseudo Code in Blocks\n')
+        with open("ShoppingList.JSON", "r") as read_file:
+            pcode = json.load(read_file)
 
-    def printOutActivities(self, activity):
-        """
-        Prints out the waterfall activity in a
-        nicely formatted output
+        for pc in pcode:
+            print('Code Block : {0}'.format(pc) )
+            codeLines = pcode[pc]
+            for l in codeLines:
+                print('\t Line : {0}'.format(l) )
 
-        :param activity: Waterfall activity object
-        """
-        name = activity['Name']
-        description = activity['Description']
-        actionsList = activity['Actions']
-        inputList = activity['InputActivities']
-        outputList = activity['OutputActivities']
-
-        print('\n\n')
-        print("*** Waterfall Activity ***\n")
-        print('Name of Activity: {0}'.format(name))
-        print('Description: {0}'.format(description))
-        print('Actions: {0}'.format(actionsList))
-        print('Inputs to \"{1}\" : {0}'.format(inputList, name))
-        print('Outputs of \"{1}\" : {0}\n'.format(outputList, name))
-        print("**************************")
-
-    def printOutActivity(self, activity, waterfallModel):
-        """
-        Finds the requested waterfall activity
-        and, if found, prints out the activity
-        in a nicely formatted output
-
-        :param activity: activity name to find
-        :param waterfallModel: waterfall activity database
-        """
-        a = self.findActivity(activity, waterfallModel)
-        if a != None:
-            self.printOutActivities(a)
-
-    def runInput(self):
-        """
-        Run program, ask user for waterfall activity name
-        Print out activity description
-        repeat until user exits
-        """
-
-        with open("m3\module3.json", "r") as read_file:
-            wfallmodel = json.load(read_file)
-
-        conti = True
-        exitList = ['DONE', 'EXIT', 'QUIT', 'STOP', 'OVER', 'OUT', 'Q']
-        while conti:
-            inp = input('\nEnter Waterfall Activity : ')
-            conti = (False == (inp.upper() in exitList))
-            if conti == True:
-                a = self.findActivity(inp, wfallmodel)
-                if a != None:
-                    self.printOutActivity(inp, wfallmodel)
-                else:
-                    print("Activity {0} Not Found".format(inp))
-
-        print('Thank you')
-
+    def GetPrintTools(self):
+        print('\nTools Used for Designs and Wireframes\n')
+        with open("Tools.JSON", "r") as read_file:
+            pTools = json.load(read_file)
+        toolList = pTools['ToolList']
+        for pc in toolList:
+            print('Tool : {0}'.format(pc) )
 
 try:
 
-    c = ceglia()
-    c.runInput()
+    c = ShoppingListApp()
+    c.GetPrintCode()
+    c.GetPrintTools()
 
 except(Exception) as e:
     print("<<< EXCEPTION >>>")
